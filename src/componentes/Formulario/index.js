60fdb87357395262085117d2dd417201bff3c18f
Leandro Zulli Silva <leandro.silva@tecnospeed.com.br>
2023-04-10 15:05:15 -0300

    Criado o componente de botao e dando inicio a tratativa de eventos

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,10 +1,12 @@
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
 import './Formulario.css';
 
 const Formulario = () => {
 
   const times = [
+    '',
     'Programação',
     'Front-end',
     'Data Science',
@@ -14,14 +16,22 @@ const Formulario = () => {
     'Inovação e Gestão'
   ]
 
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log('Form foi submetido');
+  }
+
   return (
     <section className='formulario'>
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome" />
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
-        <ListaSuspensa label="Time" itens={times} />
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <Botao>
+          Criar Card
+        </Botao>
       </form>
     </section>
   )
