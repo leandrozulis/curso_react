daf2f809422a00b55462e5eb1ca17997b3b738c0
leandrozulis <leandro_zulli@hotmail.com>
2023-04-18 23:23:31 -0300

    Criado o componente dos Colaboradores

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+  return (
+    <div className='colaborador'>
+      <div className='cabecalho'>
+        <img src='https://github.com/leandrozulis.png' alt='Leandro Zulli' />
+      </div>
+
+      <div className='rodape'>
+        <h4>Leandro Silva</h4>
+        <h5>Consultor</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Colaborador
